a135c4ccd158d268c811924d3d6d50dc725bf1e1
Ronnie Smith <rosmith@thoughtworks.com>
2017-02-02 08:40:44 -0600

    #22 | Ronnie | Testing pipeline trigger again =)

--- src/main/java/com/thoughtpay/controllers/HomeController.java
+++ src/main/java/com/thoughtpay/controllers/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController  {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String home() {
+    public String home(){
         return "home";
     }
 

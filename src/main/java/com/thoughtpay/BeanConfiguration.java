c4e460ae235b99c06e0fe801a61934179a5f9f8a
John Beaumier <jbeaumie@thoughtworks.com>
2017-02-08 17:26:31 -0800

    #0 | John | moved InternalResourceViewResolver bean from xml to Java

--- src/main/java/com/thoughtpay/BeanConfiguration.java
+++ src/main/java/com/thoughtpay/BeanConfiguration.java
@@ -3,6 +3,7 @@ package com.thoughtpay;
 import com.thoughtpay.domain.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import java.util.HashMap;
 
@@ -10,13 +11,22 @@ import java.util.HashMap;
 public class BeanConfiguration {
 
     @Bean
-    public HashMap<String, User> allUsers() {
-        HashMap<String, User> result = new HashMap<String, User>();
+    public HashMap<String, User> getAllUsers() {
+        HashMap<String, User> result = new HashMap<>();
         result.put("1", new User("1", "nwinston", "Napoleon", "Winston"));
         result.put("2", new User("2", "wsugar", "Whiskers", "Sugar"));
         result.put("3", new User("3", "sharley", "Sylvester", "Harley"));
 
         return result;
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
     }
 
 }
